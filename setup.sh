068421a266b3f459bc7cd01c4f040658471097b3
neevek <i@neevek.net>
2017-08-13 22:45:59 +0800

    minor update

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 mkdir undo_ swap_ backup_
 cp .vimrc ../.vimrc
 git submodule add https://github.com/VundleVim/Vundle.vim bundle/Vundle.vim
+git submodule update --init bundle/Vundle.vim
 vim +PluginInstall +qall
